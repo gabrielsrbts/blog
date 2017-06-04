f2a5830e41c45bf1c0e057b7aea4713d6c5c0c74
Gabriel Resende <gabrielsrbts@gmail.com>
2017-06-04 11:26:31 +0000

    Add Pages

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
-	has_many :comments
+	has_many :comments, dependent: :destroy #dependent indica para o post que quando ele for excluido que seus comentarios tambem sejam
 	validates :title, length: { minimum: 5 }, presence: true
 	validates :body, presence: true
 end

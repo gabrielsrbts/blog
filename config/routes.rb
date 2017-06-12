69ad89221e0cd1b48795a3b560303bf0dfee7e06
Gabriel Resende <gabrielsrbts@gmail.com>
2017-06-12 13:25:10 +0000

    Add Registrations Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,25 @@
 Rails.application.routes.draw do
 
+  #devise_for :users, skip: :registrations
+  #resources :posts do
+  #	resources :comments
+  #end
+
+  #root 'posts#index'
+
+  #get '/about', to: 'pages#about'
+
+if Rails.env.production?
+  devise_for :users, :controllers => { :registrations => "registrations" }
+    resources :posts do
+      resources :comments
+    end
+else
   devise_for :users
-  resources :posts do
-  	resources :comments
-  end
+    resources :posts do
+      resources :comments
+    end
+end
 
   root 'posts#index'
 
